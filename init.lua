fb0aebda2b2636adc2d6bc57d6311f5f84d0b118
Louis van Rooyen <SZ-LvanRooyen@mains-MacBook-Pro.local>
2025-04-30 08:32:38 +0200

    update with my config

--- init.lua
+++ init.lua
@@ -663,7 +663,7 @@ require('lazy').setup({
       --  - settings (table): Override the default settings passed when initializing the server.
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
-        -- clangd = {},
+        clangd = {},
         -- gopls = {},
         -- pyright = {},
         -- rust_analyzer = {},
@@ -1005,4 +1005,3 @@ require('lazy').setup({
 })
 
 -- The line beneath this is called `modeline`. See `:help modeline`
--- vim: ts=2 sts=2 sw=2 et
